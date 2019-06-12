4f569837bcadf26b2d9a0023d6e884721df4601b
Jordan Low <jordanlow.jinyi@gmail.com>
2019-06-12 22:12:15 +0800

    Create server.py

---
+++ server.py
@@ -0,0 +1,12 @@
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('',8888))
+
+sock.listen(5)
+
+while True:
+    clientsock, address = sock.accept()
+    print(f"Connection from {address} has been established")
+    message = clientsocket.recv(9999)
+    print(message.decode("utf-8"))
