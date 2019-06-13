7186e30da05b46e49563eaca41440dcaf4840db5
Jordan Low <jordanlow.jinyi@gmail.com>
2019-06-13 12:34:29 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,47 +1,50 @@
 import socket
 import os
-
-port = int(os.environ.get("PORT", 8081))
-
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(('0.0.0.0',port))
-
-print(socket.gethostbyname(socket.gethostname()), port)
-
-sock.listen(5)
-
-while True:
-    clientsock, address = sock.accept()
-    print(f"Connection from {address} has been established")
-    try:
-        data = clientsock.recv(1024).decode('utf-8')
-        
-        if not data: 
-            break
-            
-        
-
-        print("Client Says: "+ data)
-        clientsock.sendall("Server Says: Hi")
-        
-        try:
-            req = data.split(' ')[1].split('?')[0].lstrip('/')
-            
-            if req == '/':
-                req = 'index.html'
-                file = open(myfile,'rb') # open file , r => read , b => byte format
-                response = file.read()
-                file.close()
-                header = 'HTTP/1.1 200 OK\n'
-                mimetype = 'text/html'
-                header += 'Content-Type: '+str(mimetype)+'\n\n'
-                final_response = header.encode('utf-8')
-                final_response += response
-                clientsock.send(final_response)
-        except:
-            print("Oops! Something went wrong!")
  
-    except:
-            print("Oops! Something went wrong!")
-       
+HOST,PORT = '127.0.0.1', os.environ.get("PORT", 8082)
+ 
+my_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+my_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+my_socket.bind((HOST,PORT))
+my_socket.listen(1)
+ 
+print('Serving on port ',PORT)
+ 
+while True:
+    connection,address = my_socket.accept()
+    request = connection.recv(1024).decode('utf-8')
+    string_list = request.split(' ')     # Split request from spaces
+ 
+    method = string_list[0]
+    requesting_file = string_list[1]
+ 
+    print('Client request ',requesting_file)
+ 
+    myfile = requesting_file.split('?')[0] # After the "?" symbol not relevent here
+    myfile = myfile.lstrip('/')
+    if(myfile == ''):
+        myfile = 'index.html'    # Load index file as default
+ 
+    try:
+        file = open(myfile,'rb') # open file , r => read , b => byte format
+        response = file.read()
+        file.close()
+ 
+        header = 'HTTP/1.1 200 OK\n'
+ 
+        if(myfile.endswith(".jpg")):
+            mimetype = 'image/jpg'
+        elif(myfile.endswith(".css")):
+            mimetype = 'text/css'
+        else:
+            mimetype = 'text/html'
+ 
+        header += 'Content-Type: '+str(mimetype)+'\n\n'
+ 
+    except Exception as e:
+        header = 'HTTP/1.1 404 Not Found\n\n'
+        response = '<html><body><center><h3>Error 404: File not found</h3><p>Python HTTP Server</p></center></body></html>'.encode('utf-8')
+ 
+    final_response = header.encode('utf-8')
+    final_response += response
+    connection.send(final_response)
