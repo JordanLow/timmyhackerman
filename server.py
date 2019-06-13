10ac507a6383d7d8cdc5656668f4ea310f4263b0
Jordan Low <jordanlow.jinyi@gmail.com>
2019-06-13 13:27:13 +0800

    Update server.py

--- server.py
+++ server.py
@@ -12,4 +12,4 @@ def get_data():
 
 if __name__ == '__main__':
     from os import environ
-    app.run(debug=False, host='0.0.0.0', port=environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=environ.get("PORT", 5000))
