04705537c195530c67dd8c96923349d0164430cd
Jordan Low <jordanlow.jinyi@gmail.com>
2019-06-13 14:35:32 +0800

    Update server.py

--- server.py
+++ server.py
@@ -4,12 +4,11 @@ app = Flask(__name__, static_folder='static')
 
 @app.route('/', methods=['GET', 'POST'])
 def get_data():
-    data = ''
     if request.method == 'POST':
-        print('Recieved from client: {}'.format(request.data))
+        print('Recieved from client: ')
+        print(request.values)
         sys.stdout.flush()
-        data = request.data
-    return render_template('index.html', data=data)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     from os import environ
